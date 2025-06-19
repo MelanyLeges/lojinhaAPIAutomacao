c9ed8eeade429b1ae78911abd2f6bf084f3f7cee
Melany Moraes Leges <melany.leges@gmail.com>
2025-06-19 20:33:21 -0300

    Alterei a basePath para lojinha-bugada e adicionei demais arquivos que estavam pendente

--- src/test/java/dataFactory/UsuarioDataFactory.java
+++ src/test/java/dataFactory/UsuarioDataFactory.java
@@ -1,2 +1,13 @@
-package dataFactory;public class UsuarioDataFactory {
+package dataFactory;
+
+import pojo.UsuarioPojo;
+
+public class UsuarioDataFactory {
+    public static UsuarioPojo criarUsuarioAdministrador(){
+        UsuarioPojo usuario = new UsuarioPojo();
+        usuario.setUsuarioLogin("admin");
+        usuario.setUsuarioSenha("admin");
+
+        return usuario;
+    }
 }
